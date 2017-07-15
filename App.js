18a1f2c268e4c8d4b648d5d3112d027564d57131
Scott Yu <scott.tj.yu@gmail.com>
2017-07-15 16:22:50 +0800

    added react-native-debugger

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import Root from './app/containers/Root';
-import configureStore from './app/store/configureStore';
+import store from './app/store/configureStore';
+
 global.Buffer = global.Buffer || require('buffer').Buffer;
-
-const store = configureStore();
-
 
 export default class App extends React.Component {
   render() {
